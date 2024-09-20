25c937c6eacce4c1a27b7215d2cbec2f498e78c1
Dane <epicdane@gmail.com>
2024-09-19 23:29:48 -0600

    deleted Chess folder of all code cuz it's cursed

---
+++ Demo/CommandLineArgs.java
@@ -0,0 +1,22 @@
+// lecture tutorial file
+
+// static means... ?
+// void means the function doesn't return anything
+
+public class CommandLineArgs {
+    public static void main(String[] args) {
+        // System is a class. out is a variable that the console uses?
+        System.out.println("Hello!");
+
+
+        // iterated for loop
+        for (int i = 0; i < args.length: ++i) {
+            System.out.println(args[i]);
+        }
+
+        // for each loop
+        for (String a : args) {
+         System.out.println(a)
+        }
+    }
+}
