f37935044e0a268e15bbcf96db5785f01b9b5d38
Dane <epicdane@gmail.com>
2024-09-20 20:42:38 -0600

    implemented getters and set up beginning of PieceMovesCalculator

---
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -0,0 +1,14 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PieceMovesCalculator {
+    public PieceMovesCalculator() {
+
+    }
+
+    static public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        return new ArrayList<>();
+    }
+}
