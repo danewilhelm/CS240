b6dc72f76812f4884728c1a1a66452c1c6f48701
danewilhelm <epicdane@gmail.com>
2024-10-22 15:00:06 -0600

    created placeholders for endpoints in server

--- server/src/main/java/handler/ClearHandler.java
+++ server/src/main/java/handler/ClearHandler.java
@@ -1,7 +1,21 @@
 package handler;
 
+import com.google.gson.Gson;
+import result.ClearResult;
+import service.ClearService;
+import spark.Request;
+import spark.Response;
+
 public class ClearHandler {
     /*
     Handles 1 endpoint: clear
      */
+
+    public static Object handleClear(Request request, Response response) {
+        Gson serializer = new Gson();
+//        var gsonClearRequest = serializer.fromJson(request, Clear)
+        ClearResult resultObj = ClearService.clear();
+        response.status(200);
+        return serializer.toJson(resultObj);
+    }
 }
--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -2,14 +2,16 @@ package service;
 
 
 import dataaccess.*;
+import result.ClearResult;
 
 public class ClearService {
-    public static void clear() {
+    public static ClearResult clear() {
         UserMemoryDAO.instance.clear();
         GameMemoryDAO.instance.clear();
         AuthMemoryDAO.instance.clear();
 
         // if the database was not accessible, throw a DataAccessException
         // else, return nothing
+        return new ClearResult();
     }
 }
