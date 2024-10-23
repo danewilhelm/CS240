f5121573166698b6f0e0fe52384a9760f352c133
danewilhelm <epicdane@gmail.com>
2024-10-22 19:56:00 -0600

    adding files to git watch

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,42 @@
+package server;
+
+import handler.ClearHandler;
+import handler.GameHandler;
+import handler.UserHandler;
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+        establishEndpoints();
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    private void establishEndpoints() {
+        Spark.post("/user", UserHandler::handleRegister);
+        Spark.post("/session", UserHandler::handleLogin);
+        Spark.delete("/session", UserHandler::handleLogout);
+
+        Spark.get("/game", GameHandler::handleListGames);
+        Spark.post("/game", GameHandler::handleCreateGame);
+        Spark.put("/game", GameHandler::handleJoinGame);
+
+        Spark.delete("/db", ClearHandler::handleClear);
+
+
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
